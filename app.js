3c666c784d8a8a140f91fb642ded04c89af1cc69
Tejas Desai <tejadesai0531@gmail.com>
2022-05-31 22:17:56 +0530

    Adds database

--- app.js
+++ app.js
@@ -1,9 +1,80 @@
 const express = require('express')
+const req = require('express/lib/request')
 const app = express()
 
 const math = require('./math')
 
+const knex = require('./knex')
+
 app.use(express.json())
+
+
+app.get('/employee', async (req, res) => {
+
+    let empList = await knex.select('*').from('Employee')
+    res.send(empList)
+
+})
+
+app.post('/employee', async (req, res) => {
+
+    let name = req.body.name
+    let salary = req.body.salary
+    let city = req.body.city
+    let job = req.body.job
+
+    let emp = {
+        emp_name: name,
+        emp_salary: salary,
+        city: city,
+        job: job
+    }
+
+    let result = await knex('Employee').insert(emp)
+
+    res.send(result)
+})
+
+app.patch('/employee/:id', async (req, res) => {
+
+    let id = req.params.id
+
+    let name = req.body.name
+    let salary = req.body.salary
+    let city = req.body.city
+    let job = req.body.job
+
+    let emp = {
+        emp_name: name,
+        emp_salary: salary,
+        city: city,
+        job: job
+    }
+
+    let result = await knex('Employee').update(emp).where("emp_id", id)
+
+    res.send({
+        "result": result
+    })
+})
+
+app.get('/employee/:id', async (req, res) => {
+    let id = req.params.id
+
+    let result = await knex('Employee').select("*").where("emp_id", id)
+
+    res.send(result)
+})
+
+app.delete('/employee/:id', async (req, res) => {
+
+    let id = req.params.id
+
+    let result = await knex('Employee').delete().where('emp_id', id)
+
+    res.send({result: result})
+
+})
 
 app.post('/add', (req, res) => {
 
@@ -46,5 +117,6 @@ app.post('/divide', (req, res) => {
     res.send(result)
 
 })
+
 
 module.exports = app;
