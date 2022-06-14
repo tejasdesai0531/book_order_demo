3243f87453862268310871b7feeb3d3215ab34c3
Tejas Desai <tejadesai0531@gmail.com>
2022-06-14 15:27:06 +0530

    Adds auth api

--- modules/route.js
+++ modules/route.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 const employeeRouter = require('./employee/employee.route')
-const arithmeticRouter = require('./arithmetic/arithmetic.route')
+const authRouter = require('./auth/auth.route')
+const authMiddleWare = require('../middlewares/auth.middleware')
+const arithmeticRouter = require('./arithmetic/arithmetic.route');
+const { route } = require('./employee/employee.route');
 
-router.use('/employee', employeeRouter)
+router.use('/auth', authRouter)
+router.use('/employee', authMiddleWare.authCheck, employeeRouter)
 // router.use('/arithmetic', arithmeticRouter)
 
 module.exports = router;
